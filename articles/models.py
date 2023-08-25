583459cf18324af5b9a52da3900ab6d157697bc9
hyejin beck <beckhye@gmail.com>
2023-08-25 15:34:19 +0900

    어드민페이지 생성후 article,comment포스팅함

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):          # 게시판 
+    title = models.CharField(max_length=50)  # 제목 
+    content = models.TextField()            # 내용 
+
+
+class Comment(models.Model):       # 댓글 
+    content = models.TextField()    # 작성칸 
+    article = models.ForeignKey(Article,on_delete=models.CASCADE) # 
